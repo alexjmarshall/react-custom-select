d283956a4820345c3debc1d9f7b79174cdd3b067
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2020-10-02 19:54:50 -0600

    cleanup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CustomSelect from './CustomSelect';
 
 function App() {
-  const options = ["banana","apple","orange","cherry","mango","watermelon","kiwi","pear"];
+  const options = ['banana','apple','orange','cherry','mango','watermelon','kiwi','pear'];
 
   return (
     <CustomSelect options={options} label={'Choose a fruit: '} uniqueId={Math.ceil(Math.random() * 100)}></CustomSelect>
